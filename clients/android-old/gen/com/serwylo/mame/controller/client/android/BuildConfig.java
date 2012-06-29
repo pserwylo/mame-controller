e0886261f63a298ef75b8f1f48fd479d70708974
Peter Serwylo <peter@ivt.com.au>
2012-06-30 09:49:54 +1000

    Removed old gdx stuff.

--- clients/android-old/gen/com/serwylo/mame/controller/client/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.serwylo.mame.controller.client.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
